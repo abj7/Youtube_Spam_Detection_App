b0acbcea2817faa6cec5fe7a4299c9e1ef3a6e83
Alan <abj3@rice.edu>
2019-01-29 21:26:41 -0600

    youtube api changes

--- Youtube-Spam-Detector-ML-Flask-App/app.py
+++ Youtube-Spam-Detector-ML-Flask-App/app.py
@@ -1,8 +1,13 @@
-from flask import Flask,render_template,url_for,request
+from flask import Flask,render_template, request
 from sklearn.externals import joblib
+import argparse
+import youtube as yt
 
+app = Flask(__name__, static_url_path='')
 
-app = Flask(__name__)
+@app.route('/home/')
+def root():
+    return app.send_static_file('home.html')
 
 @app.route('/')
 def home():
@@ -23,6 +28,30 @@ def predict():
 def back():
     return render_template('home.html')
 
+@app.route('/submit', methods = ['POST'])
+def submit():
+    ytb_model = open("YTSpam_model.pkl", "rb")
+    (clf, cv) = joblib.load(ytb_model)
+    if request.method == 'POST':
+        id = yt.parse_video_id(request.form['comment'])
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--videoid", dest='videoid', default="L-oNKK1CrnU", help="ID of video to like.")
+        args = parser.parse_args()
+
+        youtube = yt.get_authenticated_service(args)
+
+        data = yt.get_comments(youtube, id)
+        vect = cv.transform(data).toarray()
+        my_prediction = clf.predict(vect)
+        spam_comments = []
+        print (id)
+        print (data)
+        print (my_prediction)
+        for elem in range(0, len(data)):
+            if my_prediction[elem] == 1:
+                spam_comments.append(data[elem])
+    return render_template('comments.html', comment = spam_comments)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ Youtube-Spam-Detector-ML-Flask-App/monitor.py
@@ -0,0 +1,31 @@
+from sklearn.externals import joblib
+import argparse
+import youtube as yt
+from oauth2client.tools import argparser, run_flow
+
+ytb_model = open("YTSpam_model.pkl", "rb")
+(clf, cv) = joblib.load(ytb_model)
+id = yt.parse_video_id("https://www.youtube.com/watch?v=nqMYG2Riq54&list=PLHEN7nVUglIRGw5xRC4L-RXwudJqyCKlU&index=14")
+# The "videoid" option specifies the YouTube video ID that uniquely
+  # identifies the video for which the comment will be inserted.
+argparser.add_argument("videoid",
+    help="Required; ID for video for which the comment will be inserted.")
+  # The "text" option specifies the text that will be used as comment.
+args = argparser.parse_args()
+
+if not args.videoid:
+    exit("Please specify videoid using the --videoid= parameter.")
+if not args.text:
+    exit("Please specify text using the --text= parameter.")
+
+youtube = yt.get_authenticated_service(args)
+data = yt.get_comments(youtube, id)
+vect = cv.transform(data).toarray()
+my_prediction = clf.predict(vect)
+spam_comments = []
+print (id)
+print (data)
+print (my_prediction)
+for elem in range(0, len(data)):
+    if my_prediction[elem] == 1:
+        spam_comments.append(data[elem])
